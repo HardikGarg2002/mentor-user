3991d13d2ea4172c8eac204356c5075d2222fb3c
hardikgarg2002 <hardikgarg3085@gmail.com>
2025-05-04 13:24:32 +0530

    restructured and refactored session card components

---
+++ components/dashboard/sessioncard/session-card.tsx
@@ -0,0 +1,63 @@
+import { MenteeCardActions } from "./mentee-card-actions";
+import { MentorCardActions } from "./mentor-card-actions";
+import { SessionDetails } from "./session-details";
+
+interface SessionCardProps {
+  id: string;
+  name: string;
+  image: string;
+  title: string;
+  type: string;
+  sessionDate: string | Date;
+  sessionStatus: string;
+  sessionStartTime: string;
+  sessionDuration: number;
+  rating: number;
+  isUpcoming: boolean;
+  isMentor: boolean;
+}
+
+export function SessionCard({
+  id,
+  name,
+  image,
+  title,
+  type,
+  sessionDate,
+  sessionStatus,
+  sessionStartTime,
+  sessionDuration,
+  rating,
+  isUpcoming,
+  isMentor,
+}: SessionCardProps) {
+  return (
+    <div className="flex flex-col md:flex-row md:items-center justify-between p-4 border rounded-lg gap-4">
+      <SessionDetails
+        name={name}
+        title={title}
+        image={image}
+        sessionDate={sessionDate}
+        sessionStartTime={sessionStartTime}
+        sessionDuration={sessionDuration}
+        isUpcoming={isUpcoming}
+        rating={rating}
+      />
+      {isMentor ? (
+        <MentorCardActions
+          id={id}
+          isUpcoming={isUpcoming}
+          sessionStatus={sessionStatus}
+          sessionType={type}
+        />
+      ) : (
+        <MenteeCardActions
+          id={id}
+          isUpcoming={isUpcoming}
+          sessionStatus={sessionStatus}
+          type={type}
+        />
+      )}
+    </div>
+  );
+}
